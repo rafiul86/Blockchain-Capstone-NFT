75c909fe50bae296a74b9eb1239ba233da6fd3f7
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-07-25 20:06:33 +0600

    ERC721Custom contract implemented

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -479,9 +479,9 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
     
     // TODO: Create private vars for token _name, _symbol, and _baseTokenURI (string)
 
-    string private _name = "Serenity Token";
-    string private _symbol = "SRT";
-    string private _baseTokenURI = "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/";
+    string private _name;
+    string private _symbol;
+    string private _baseTokenURI;
     // TODO: create private mapping of tokenId's to token uri's called '_tokenURIs'
         struct  Token{
                 string name;
@@ -537,13 +537,28 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
 }
 
 //  TODO's: Create CustomERC721Token contract that inherits from the ERC721Metadata contract. You can name this contract as you please
+contract ERC721MintableComplete is ERC721Metadata{
+    string private _name;
+    string private _symbol;
 //  1) Pass in appropriate values for the inherited ERC721Metadata contract
 //      - make the base token uri: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/
+    constructor(string memory name, string memory symbol) 
+    ERC721Metadata(name, symbol, "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/") 
+
+    public
+    {
+         _name = name;
+         _symbol = symbol;
+    }
 //  2) create a public mint() that does the following:
 //      -can only be executed by the contract owner
 //      -takes in a 'to' address, tokenId, and tokenURI as parameters
 //      -returns a true boolean upon completion of the function
 //      -calls the superclass mint and setTokenURI functions
-
+function mint(address to, uint256 tokenId) public onlyOwner whenNotPaused returns(bool){
+        super._mint(to, tokenId);
+        setTokenURI(tokenId);
+    }
+}
 
 
