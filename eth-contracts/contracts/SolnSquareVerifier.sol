c47d0b3aa6d71af9be6d6b0513b1ab0d63872c17
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-07-26 12:29:40 +0600

    SolnSquareVerifier contract implemented

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -24,26 +24,27 @@ contract SolnSquareVerifier is ERC721MintableComplete{
     }
 // TODO define a solutions struct that can hold an index & an address
 struct Solution{
-    uint256 index;
+    bytes32 index;
     address account;
+    bool isExist;
 }
 
 // TODO define an array of the above struct
 
 
 // TODO define a mapping to store unique solutions submitted
-mapping (uint => Solution) uniqueSolutions;
+mapping (bytes32 => Solution) uniqueSolutions;
 
 
 // TODO Create an event to emit when a solution is added
-event SolutionAdded(uint256 index, address account);
+event SolutionAdded(bytes32 index, address account);
 
-// bytes32 key = keccak256(abi.encodePacked(index, airline, flight, timestamp));
+ 
 // TODO Create a function to add the solutions to the array and emit the event
-function addSolutionsToArray(uint256 index, address sender) public returns(bool){
-    require(uniqueSolutions[index].account != sender, "Solution is not unique.");
-    uniqueSolutions[index] = Solution({index: index, account: sender});
-    emit SolutionAdded(index, sender);
+function addSolutionsToArray(bytes32 key) public returns(bool){
+    require(uniqueSolutions[key].account != msg.sender, "Solution is not unique.");
+    uniqueSolutions[key] = Solution({index: key, account: msg.sender, isExist: true});
+    emit SolutionAdded(key, msg.sender);
     return true;
 }
 
@@ -60,14 +61,21 @@ function mintToken(
              public
              returns (bool)
               {
-//  - make sure the solution is unique (has not been used before)
-    require(squareVerifier.verifyTx(a, b, c, input), "Can't mint a new token, Verification is failed");
-    require(addSolutionsToArray(index, msg.sender), "solution used is not unique");
-//  - make sure you handle metadata as well as tokenSuplly
-    require(index < totalSupply(), "Invalid Token index");
-    mint(msg.sender,tokenId);
-    setTokenURI(tokenId);
-    return true;
+
+    bytes32 key = keccak256(abi.encodePacked(a, b, c, input));
+
+    if(!uniqueSolutions[key].isExist){
+      bool verification = squareVerifier.verifyTx(a, b, c,input);
+
+        if(verification){
+            addSolutionsToArray(key);
+            require(index < totalSupply(), "Invalid Token index");
+            mint(msg.sender,tokenId);
+            //  - make sure you handle metadata as well as tokenSuplly
+            setTokenURI(tokenId);
+            return true;
+        }
+     }
     }
 }
 // //  TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
@@ -79,32 +87,3 @@ function mintToken(
 //         uint[2] memory input
 //         ) public returns (bool r);
 // }
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
